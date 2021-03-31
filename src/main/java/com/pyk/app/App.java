9eaac49a29535050c67f605019c20b9bdc50a0a5
pyk1 <arrav7@gmail.com>
2021-03-31 17:01:06 +0530

    mvn

---
+++ src/main/java/com/pyk/app/App.java
@@ -0,0 +1,13 @@
+package com.pyk.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
